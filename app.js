6b082daf91c4d455d81f7355f595d9f68b4e9d77
Christian Heller <christian.heller@sap.com>
2018-04-10 17:03:48 +0200

    implemented iot services 4.0 connection

---
+++ app.js
@@ -0,0 +1,49 @@
+const Hue = require('./hue');
+const IoTService = require('./iotservice');
+
+const fs = require('fs');
+
+const HUE_BRIDGE = {
+    IP: '192.168.8.102',
+    USER: '3DHj4Ynx6eqovFpksBUtPlUyOfQ15teZ-iljTpB-'
+};
+
+const IOT = {
+    TENANT_ID: "hack2sol-master",
+    CERTIFICATE: {
+        KEY: fs.readFileSync('./certificate/key', 'utf8'),
+        CERT: fs.readFileSync('./certificate/cert', 'utf8'),
+        PASSPHRASE: fs.readFileSync('./certificate/passphrase', 'utf8')
+    }
+};
+
+const DEVICE = {
+    ID: "f833f3c1-ea3e-4681-b865-1d18e80c026e",
+    SENSOR_ALTERNATE_ID: "2482d776-4d37-43c4-93a0-bad9638ccdd1",
+    CAPABILITY_ALTERNATE_ID: "bfebef751b9bf768",
+};
+
+function getMeasure() {
+    return {
+        "on": true,
+        "bri": 255,
+        "hue": 30000,
+        "sat": 200,
+        "reachable": true
+    };
+}
+
+function main() {
+    let hue = new Hue(HUE_BRIDGE.IP, HUE_BRIDGE.USER);
+    let iotService = new IoTService(IOT.TENANT_ID, DEVICE.ID, IOT.CERTIFICATE.KEY, IOT.CERTIFICATE.CERT, IOT.CERTIFICATE.PASSPHRASE);
+
+    iotService.connect(function () {
+        setInterval(function () {
+            iotService.publish(DEVICE.ID, DEVICE.SENSOR_ALTERNATE_ID, DEVICE.CAPABILITY_ALTERNATE_ID,
+                [getMeasure()]
+            );
+        }, 1000);
+    });
+}
+
+main();
--- hue.js
+++ hue.js
@@ -1,121 +1,115 @@
-var request = require('request');
- 
-function Hue(ip, user) {
-    this.api = "http://" + ip + "/api/" + user;
-};
- 
-Hue.prototype.COLOR_XY = {
-    RED: [0.7006, 0.2993],
-    GREEN: [0.1724, 0.7468],
-    BLUE: [0.1355, 0.03988],
-    WHITE: [0.3227, 0.329],
-    WHITE_RELAXED: [0.4281, 0.4009]
-};
- 
-Hue.prototype.getLights = function (cb) {
-    this._api_get("/lights/", cb);
-};
- 
-Hue.prototype.setLightState = function (light, lightState) {
-    this._api_put("/lights/" + light + "/state", lightState, cb);
-};
- 
-Hue.prototype.setAllLightState = function (lightState, cb) {
-    this._api_put("/groups/0/action", lightState, cb);
-};
- 
-Hue.prototype.turnAllOn = function (lightState) {
-    this.setAllLightState({
-        on: true
-    });
-};
- 
-Hue.prototype.turnAllOff = function (lightState) {
-    this.setAllLightState({
-        on: false
-    });
-};
- 
-Hue.prototype.setColor = function (xyColor, bri, cb) {
-    this.setAllLightState({
-        bri: bri || 254,
-        xy: xyColor,
-        transitiontime: 0
-    }, function () {
-        if (cb)
-            cb();
-    });
-};
- 
-Hue.prototype.fadeAllTo = function (xyColor, ms, bri, cb) {
-    bri = bri || 254;
-    ms = ms || 1000;
- 
-    this.setAllLightState({
-        bri: bri,
-        xy: xyColor,
-        transitiontime: ms / 100
-    }, function () {
-        setTimeout(function () {
+const request = require('request');
+
+class Hue {
+    constructor(ip, user) {
+        this.api = `http://${ip}/api/${user}`;
+    }
+
+    getColors() {
+        return {
+            RED: [0.7006, 0.2993],
+            GREEN: [0.1724, 0.7468],
+            BLUE: [0.1355, 0.03988],
+            WHITE: [0.3227, 0.329],
+            WHITE_RELAXED: [0.4281, 0.4009]
+        };
+    }
+
+    getLights(cb) {
+        this._api_get("/lights/", cb);
+    }
+
+    setLightState(light, lightState) {
+        this._api_put(`/lights/${light}/state`, lightState, cb);
+    }
+
+    setAllLightState(lightState, cb) {
+        this._api_put("/groups/0/action", lightState, cb);
+    }
+
+    turnAllOn(lightState, cb) {
+        this.setAllLightState({
+            on: true
+        }, cb);
+    }
+
+    turnAllOff(lightState, cb) {
+        this.setAllLightState({
+            on: false
+        }, cb);
+    }
+
+    setColor(xyColor, bri, cb) {
+        this.setAllLightState({
+            bri: bri || 254,
+            xy: xyColor,
+            transitiontime: 0
+        }, () => {
             if (cb)
                 cb();
-        }, ms);
-    });
-};
- 
- 
-/* http helper functions */
- 
-Hue.prototype._api_get = function (path, cb) {
-    var url = this.api + path;
-    request.get({
-        url: url,
-        json: true
-    }, function (err, res) {
-        if (cb)
-            err ? cb(err) : cb(null, res.body);
-    });
-};
- 
-Hue.prototype._api_put = function (path, payload, cb) {
-    var url = this.api + path;
-    request.put({
-        url: url,
-        json: true,
-        body: payload
-    }, function (err, res) {
-        if (cb)
-            err ? cb(err) : cb(null, res.body);
-    });
-};
- 
-Hue.prototype.getXYFromRGB = function (red, green, blue) {
-    //Gamma correction
-    red = (red > 0.04045) ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : (red / 12.92);
-    green = (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
-    blue = (blue > 0.04045) ? Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4) : (blue / 12.92);
- 
-    //Apply wide gamut conversion D65
-    var X = red * 0.664511 + green * 0.154324 + blue * 0.162028;
-    var Y = red * 0.283881 + green * 0.668433 + blue * 0.047685;
-    var Z = red * 0.000088 + green * 0.072310 + blue * 0.986039;
- 
-    var fx = X / (X + Y + Z);
-    var fy = Y / (X + Y + Z);
- 
-    if (isNaN(fx))
-        fx = 0.0;
-    if (isNaN(fy))
-        fy = 0.0;
- 
-    return [parseFloat(fx.toPrecision(4)), parseFloat(fy.toPrecision(4))];
+        });
+    }
+
+    fadeAllTo(xyColor, ms = 1000, bri = 254, cb) {
+        this.setAllLightState({
+            bri,
+            xy: xyColor,
+            transitiontime: ms / 100
+        }, () => {
+            setTimeout(() => {
+                if (cb)
+                    cb();
+            }, ms);
+        });
+    }
+
+    /* http helper functions */
+
+    _api_get(path, cb) {
+        const url = this.api + path;
+        request.get({
+            url,
+            json: true
+        }, (err, res) => {
+            if (cb)
+                err ? cb(err) : cb(null, res.body);
+        });
+    }
+
+    _api_put(path, payload, cb) {
+        const url = this.api + path;
+        request.put({
+            url,
+            json: true,
+            body: payload
+        }, (err, res) => {
+            if (cb)
+                err ? cb(err) : cb(null, res.body);
+        });
+    }
+
+    getXYFromRGB(red, green, blue) {
+        //Gamma correction
+        red = (red > 0.04045) ? (red + 0.055) / (1.0 + 0.055) ** 2.4 : (red / 12.92);
+        green = (green > 0.04045) ? (green + 0.055) / (1.0 + 0.055) ** 2.4 : (green / 12.92);
+        blue = (blue > 0.04045) ? (blue + 0.055) / (1.0 + 0.055) ** 2.4 : (blue / 12.92);
+
+        //Apply wide gamut conversion D65
+        const X = red * 0.664511 + green * 0.154324 + blue * 0.162028;
+        const Y = red * 0.283881 + green * 0.668433 + blue * 0.047685;
+        const Z = red * 0.000088 + green * 0.072310 + blue * 0.986039;
+
+        let fx = X / (X + Y + Z);
+        let fy = Y / (X + Y + Z);
+
+        if (isNaN(fx))
+            fx = 0.0;
+        if (isNaN(fy))
+            fy = 0.0;
+
+        return [parseFloat(fx.toPrecision(4)), parseFloat(fy.toPrecision(4))];
+    }
 }
- 
-module.exports = Hue;
- 
- 
- //test code
-var HUE_BRIDGE_IP = '192.168.8.102';
-var HUE_USER = '3DHj4Ynx6eqovFpksBUtPlUyOfQ15teZ-iljTpB-';
- 
-var hue = new Hue(HUE_BRIDGE_IP, HUE_USER);
+
+module
+    .exports = Hue;
